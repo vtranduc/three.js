4cc66b840d475541ea4622168c9e782f3164d1bc
Stephen Gou <sgou@spinvfx.com>
2017-03-20 15:57:39 -0400

    remove codes no longer needed from stereo camera
    remove codes no longer needed from stereo camera

--- src/cameras/StereoCamera.js
+++ src/cameras/StereoCamera.js
@@ -21,47 +21,9 @@ function StereoCamera() {
 	this.cameraR = new PerspectiveCamera();
 	this.cameraR.layers.enable( 2 );
 	this.cameraR.matrixAutoUpdate = false;
-
-	this.baseProjectionMatrix;
-	this.baseMatrixWorld;
-	this.baseFocus;
-	this.baseFov;
-	this.baseAspect;
-	this.baseNear;
-	this.baseFar;
-	this.baseZoom;
 }
 
 Object.assign( StereoCamera.prototype, {
-	setPerEyeViewOffset: function (xOffset, yOffset) {
-		var eyeRight = new Matrix4();
-		var eyeLeft = new Matrix4();
-
-		var projectionMatrix = this.baseProjectionMatrix.clone();
-		var eyeSep = this.eyeSep / 2;
-		var eyeSepOnProjection = eyeSep * this.near / this.focus;
-		var ymax = ( this.near * Math.tan( _Math.DEG2RAD * this.fov * 0.5 ) ) / this.zoom;
-		var xmin, xmax;
-		var aspect = this.baseAspect * this.aspect;
-
-		// translate xOffset
-
-		eyeLeft.elements[ 12 ] = - eyeSep;
-
-		//eyeLeft.elements[ 12 ] = - eyeSep + xOffset;
-		//eyeLeft.elements[ 13 ] = yOffset;
-		// for left eye
-
-		xmin = - ymax * aspect + eyeSepOnProjection;
-		xmax = ymax * aspect + eyeSepOnProjection;
-
-		projectionMatrix.elements[ 0 ] = 2 * this.near / ( xmax - xmin );
-		projectionMatrix.elements[ 8 ] = ( xmax + xmin ) / ( xmax - xmin );
-
-		this.cameraL.projectionMatrix.copy( projectionMatrix );
-		this.cameraL.matrixWorld.copy( 	this.baseMatrixWorld ).multiply( eyeLeft );
-	},
-	
 	update: ( function () {
 
 		var instance, focus, fov, aspect, near, far, zoom;
@@ -87,15 +49,6 @@ Object.assign( StereoCamera.prototype, {
 				far = camera.far;
 				zoom = camera.zoom;
 
-				//copy properties to object
-				this.baseProjectionMatrix = camera.projectionMatrix.clone();
-				this.baseMatrixWorld = camera.matrixWorld.clone();
-				this.baseFocus = camera.focus;
-				this.baseFov = camera.fov;
-				this.baseAspect = camera.aspect;
-				this.baseNear = camera.near;
-				this.baseFar = camera.far;
-				this.baseZoom = camera.zoom;
 				// Off-axis stereoscopic effect based on
 				// http://paulbourke.net/stereographics/stereorender/
 
