702d806c34b2ff33997d63edd22d49707e2c7572
Jordan Munroe <jmunroe@spinvfx.com>
2017-04-12 16:17:24 -0400

    reverted fogexp2 temp changes.

--- src/scenes/FogExp2.js
+++ src/scenes/FogExp2.js
@@ -5,17 +5,12 @@ import { Color } from '../math/Color';
  * @author alteredq / http://alteredqualia.com/
  */
 
-function FogExp2 ( color, density, distanceNear, distanceFar, heightNear, heightFar ) {
+function FogExp2 ( color, density ) {
 
 	this.name = '';
 
 	this.color = new Color( color );
-	this.density = ( density !== undefined ) ? density : 1;
-
-	this.distanceNear = ( distanceNear !== undefined ) ? distanceNear : 1;
-	this.distanceFar = ( distanceFar !== undefined ) ? distanceFar : 1000;
-	this.heightNear = ( heightNear !== undefined ) ? heightNear : 1;
-	this.heightFar = ( heightFar !== undefined ) ? heightFar : 1000;
+	this.density = ( density !== undefined ) ? density : 0.00025;
 
 }
 
@@ -23,7 +18,7 @@ FogExp2.prototype.isFogExp2 = true;
 
 FogExp2.prototype.clone = function () {
 
-	return new FogExp2( this.color.getHex(), this.density, this.distanceNear, this.distanceFar, this.heightNear, this.heightFar );
+	return new FogExp2( this.color.getHex(), this.density );
 
 };
 
@@ -32,11 +27,7 @@ FogExp2.prototype.toJSON = function ( meta ) {
 	return {
 		type: 'FogExp2',
 		color: this.color.getHex(),
-		density: this.density,
-		distanceNear: this.distanceNear,
-		distanceFar: this.distanceFar,
-		heightNear: this.heightNear,
-		heightFar: this.heightFar
+		density: this.density
 	};
 
 };
