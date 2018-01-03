3e05fdf6b3c2f368498bb10b7d471c48dbc80533
sgottfried <sgottfried@spinvfx.com>
2018-01-03 10:53:40 -0500

    removed some GLSL conditionals to fix tri planar tiling issue for light maps and AO when tri planar projection is enabled

--- src/renderers/shaders/ShaderChunk/aomap_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/aomap_fragment.glsl
@@ -1,9 +1,5 @@
 #ifdef USE_AOMAP
-	#ifdef USE_TRIPLANAR
-	float ambientOcclusion = ( (enableProjection ? GetTexelColorFromProjection(aoMap, vProjectionPosition) : texture2D( aoMap, vUv2 )).r - 1.0 ) * aoMapIntensity + 1.0;
-	#else
 	float ambientOcclusion = ( texture2D( aoMap, vUv2 ).r - 1.0 ) * aoMapIntensity + 1.0;
-	#endif
 	reflectedLight.indirectDiffuse *= ambientOcclusion;
 
 	#if defined( USE_ENVMAP ) && defined( PHYSICAL )
