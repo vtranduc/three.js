fea6d819683988d5fee908187dd1c0e36d0bed7a
Jordan Munroe <jmunroe@spinvfx.com>
2018-01-31 12:05:49 -0500

    equirect background support.

--- src/renderers/webgl/WebGLBackground.js
+++ src/renderers/webgl/WebGLBackground.js
@@ -41,7 +41,7 @@ function WebGLBackground( renderer, state, geometries, premultipliedAlpha ) {
 
 		}
 
-		if ( background && background.isCubeTexture ) {
+		if ( background && ( background.isCubeTexture || background.isRenderTargetCubeTexture ) ) {
 
 			if ( boxMesh === undefined ) {
 
