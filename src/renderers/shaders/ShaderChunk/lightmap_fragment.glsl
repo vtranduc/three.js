83db094117ef7c893c5e5dde2cc499c86d4bca50
Matthew Robertson <mrobertson@spinvfx.com>
2018-10-04 14:56:15 -0400

    Changes Lightmap blending
    Removes the factor of PI in the lightmap addition, and changes the lightmap contribution to depend on
    the texel colour (via multiplying), providing a much more realistic result.

--- src/renderers/shaders/ShaderChunk/lightmap_fragment.glsl
+++ src/renderers/shaders/ShaderChunk/lightmap_fragment.glsl
@@ -1,5 +1,5 @@
 #ifdef USE_INDIRECT_LIGHTMAP
 
-	reflectedLight.indirectDiffuse += PI * texture2D( indirectLightMap, vUv2 ).xyz * indirectLightMapIntensity; // factor of PI should not be present; included here to prevent breakage
+	reflectedLight.indirectDiffuse += texture2D( indirectLightMap, vUv2 ).xyz * diffuseColor.rgb * indirectLightMapIntensity;
 
 #endif
