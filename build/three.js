dec75f4c2eb8a3c05d38fa608b530482cd65ca46
Matthew Robertson <mrobertson@spinvfx.com>
2018-11-26 15:13:10 -0500

    Small Performance Improvement
    Removes some extraneous memory assignments from the directional light code.

--- build/three.js
+++ build/three.js
@@ -35360,6 +35360,9 @@
 
 		this.shadow = new DirectionalLightShadow();
 		this.shadowCascade = [new DirectionalLightShadow(), new DirectionalLightShadow(), new DirectionalLightShadow()];
+		this.spareMatrix = new THREE.Matrix4();
+		this.workMinVec = new THREE.Vector2();
+		this.workMaxVec = new THREE.Vector2();
 
 	}
 
@@ -35373,29 +35376,32 @@
 			// Update our shadow camera frustum to fit the viewing frustum
 			cameraFrusta.map( _.bind( function ( cameraFrustumVerts, index ) {
 				var transformedBox = sceneBoundingBox.clone();
-				var min = null;
-				var max = null;
+				var firstVert = true;
 				this.shadowCascade[index].camera.updateMatrixWorld();
-				var inv = new THREE.Matrix4().getInverse(this.shadowCascade[index].camera.matrixWorld);
-				cameraFrustumVerts.map( function ( frustumVert ) {
+				var inv = this.spareMatrix.getInverse(this.shadowCascade[index].camera.matrixWorld);
+				for (var i = 0; i < cameraFrustumVerts.length; i++) {
+					var frustumVert = cameraFrustumVerts[i];
 					frustumVert.applyMatrix4(inv);
-					if (min) {
-						min.x = Math.min(frustumVert.x, min.x);
-						min.y = Math.min(frustumVert.y, min.y);
-						max.x = Math.max(frustumVert.x, max.x);
-						max.y = Math.max(frustumVert.y, max.y);
+					if (!firstVert) {
+						this.workMinVec.x = Math.min(frustumVert.x, this.workMinVec.x);
+						this.workMinVec.y = Math.min(frustumVert.y, this.workMinVec.y);
+						this.workMaxVec.x = Math.max(frustumVert.x, this.workMaxVec.x);
+						this.workMaxVec.y = Math.max(frustumVert.y, this.workMaxVec.y);
 					} else {
-						min = new THREE.Vector2(frustumVert.x, frustumVert.y);
-						max = new THREE.Vector2(frustumVert.x, frustumVert.y);
+						this.workMinVec.x = frustumVert.x;
+						this.workMinVec.y = frustumVert.y;
+						this.workMaxVec.x = frustumVert.x;
+						this.workMaxVec.y = frustumVert.y;
+						firstVert = false;
 					}
-				});
+				}
 
 				// We limit our shadow cam to fit the scene or to the calculated frustum, whichever is a tighter bound.
 				transformedBox.applyMatrix4(inv);
-				this.shadowCascade[index].camera.left = Math.max(min.x, transformedBox.min.x);
-				this.shadowCascade[index].camera.bottom = Math.max(min.y, transformedBox.min.y);
-				this.shadowCascade[index].camera.right = Math.min(max.x, transformedBox.max.x);
-				this.shadowCascade[index].camera.top = Math.min(max.y, transformedBox.max.y);
+				this.shadowCascade[index].camera.left = Math.max(this.workMinVec.x, transformedBox.min.x);
+				this.shadowCascade[index].camera.bottom = Math.max(this.workMinVec.y, transformedBox.min.y);
+				this.shadowCascade[index].camera.right = Math.min(this.workMaxVec.x, transformedBox.max.x);
+				this.shadowCascade[index].camera.top = Math.min(this.workMaxVec.y, transformedBox.max.y);
 
 				this.shadowCascade[index].camera.far = Math.max(0, -transformedBox.min.z);
 				this.shadowCascade[index].camera.updateProjectionMatrix();
