ebdbd601238d8c97a9ca1b2c0d936f5307b274e1
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-16 16:20:23 -0400

    modified pmrem generation to allow for variable resolution and flipped ouput.

--- src/renderers/webgl/WebGLTextures.js
+++ src/renderers/webgl/WebGLTextures.js
@@ -326,7 +326,7 @@ function WebGLTextures( _gl, extensions, state, properties, capabilities, utils,
 
 			if ( image === undefined ) {
 
-				console.warn( 'THREE.WebGLRenderer: Texture marked for update but image is undefined' );
+				if ( texture.mapping !== THREE.CubeUVReflectionMapping ) console.warn( 'THREE.WebGLRenderer: Texture marked for update but image is undefined' );
 
 			} else if ( image.complete === false ) {
 
