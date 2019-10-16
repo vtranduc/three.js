fd8d45b5ed91204740682eb0213e63efdca7848b
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-16 15:09:47 -0400

    add method to CubeTexture to convert from equirectangular

--- src/textures/CubeTexture.js
+++ src/textures/CubeTexture.js
@@ -3,7 +3,12 @@
  */
 
 import { Texture } from './Texture.js';
-import { CubeReflectionMapping, RGBFormat } from '../constants.js';
+import { CubeReflectionMapping, RGBFormat, BackSide, RepeatWrapping } from '../constants.js';
+import { Scene } from '../scenes/Scene';
+import { CubeCamera } from '../cameras/CubeCamera'
+import { MeshBasicMaterial } from '../materials/MeshBasicMaterial'
+import { Mesh } from '../objects/Mesh'
+import { SphereBufferGeometry } from '../geometries/SphereGeometry'
 
 function CubeTexture( images, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy, encoding ) {
 
@@ -37,6 +42,36 @@ Object.defineProperty( CubeTexture.prototype, 'images', {
 	}
 
 } );
+
+CubeTexture.prototype.fromEquirectangular = function( renderer, source, size, detail ) {
+
+	var scene = new Scene();
+
+	var gl = renderer.getContext();
+	var maxSize = gl.getParameter( gl.MAX_CUBE_MAP_TEXTURE_SIZE )
+
+	var camera = new CubeCamera( 1, 100000, Math.min( size, maxSize ) );
+
+	source.wrapS = source.wrapT = RepeatWrapping;
+
+	var material = new MeshBasicMaterial( {
+		map: source,
+		side: BackSide
+	} );
+
+	var mesh = new Mesh(
+		new SphereBufferGeometry( 100, 10 * ( detail || 3 ), 10 * ( detail || 3 ) ),
+		material
+	);
+
+	scene.add( mesh );
+
+	camera.update( renderer, scene );
+	camera.renderTarget.texture.isRenderTargetCubeTexture = true;
+
+	return camera.renderTarget.texture;
+
+}
 
 
 export { CubeTexture };
