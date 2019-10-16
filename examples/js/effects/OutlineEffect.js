3542992e843205c1fc35ffbc94e1a0e154c6bb77
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-16 16:46:46 -0400

    modified OutlineEffect to take a three Color as the color input, and to ignore scene fog.

--- examples/js/effects/OutlineEffect.js
+++ examples/js/effects/OutlineEffect.js
@@ -41,7 +41,7 @@
  * // How to set default outline parameters
  * new THREE.OutlineEffect( renderer, {
  * 	defaultThickness: 0.01,
- * 	defaultColor: [ 0, 0, 0 ],
+ * 	defaultColor: new THREE.Color( 0x888888 ),
  * 	defaultAlpha: 0.8,
  * 	defaultKeepAlive: true // keeps outline material in cache even if material is removed from scene
  * } );
@@ -49,7 +49,7 @@
  * // How to set outline parameters for each material
  * material.userData.outlineParameters = {
  * 	thickness: 0.01,
- * 	color: [ 0, 0, 0 ]
+ * 	color: new THREE.Color( 0x888888 ),
  * 	alpha: 0.8,
  * 	visible: true,
  * 	keepAlive: true
@@ -66,7 +66,7 @@ THREE.OutlineEffect = function ( renderer, parameters ) {
 	this.enabled = true;
 
 	var defaultThickness = parameters.defaultThickness !== undefined ? parameters.defaultThickness : 0.003;
-	var defaultColor = new THREE.Color().fromArray( parameters.defaultColor !== undefined ? parameters.defaultColor : [ 0, 0, 0 ] );
+	var defaultColor = parameters.defaultColor !== undefined ? parameters.defaultColor : new THREE.Color( 0x000000 );
 	var defaultAlpha = parameters.defaultAlpha !== undefined ? parameters.defaultAlpha : 1.0;
 	var defaultKeepAlive = parameters.defaultKeepAlive !== undefined ? parameters.defaultKeepAlive : false;
 
@@ -276,7 +276,7 @@ THREE.OutlineEffect = function ( renderer, parameters ) {
 
 	function setOutlineMaterial( object ) {
 
-		if ( object.material === undefined ) return;
+		if ( object.material === undefined || object.isSprite ) return;
 
 		if ( Array.isArray( object.material ) ) {
 
@@ -299,7 +299,7 @@ THREE.OutlineEffect = function ( renderer, parameters ) {
 
 	function restoreOriginalMaterial( object ) {
 
-		if ( object.material === undefined ) return;
+		if ( object.material === undefined || object.isSprite ) return;
 
 		if ( Array.isArray( object.material ) ) {
 
@@ -332,14 +332,14 @@ THREE.OutlineEffect = function ( renderer, parameters ) {
 
 	function updateUniforms( material, originalMaterial ) {
 
-		var outlineParameters = originalMaterial.userData.outlineParameters;
+		var outlineParameters = originalMaterial.outlineParameters;
 
 		material.uniforms.outlineAlpha.value = originalMaterial.opacity;
 
 		if ( outlineParameters !== undefined ) {
 
 			if ( outlineParameters.thickness !== undefined ) material.uniforms.outlineThickness.value = outlineParameters.thickness;
-			if ( outlineParameters.color !== undefined ) material.uniforms.outlineColor.value.fromArray( outlineParameters.color );
+			if ( outlineParameters.color !== undefined ) material.uniforms.outlineColor.value.copy( outlineParameters.color );
 			if ( outlineParameters.alpha !== undefined ) material.uniforms.outlineAlpha.value = outlineParameters.alpha;
 
 		}
@@ -350,7 +350,7 @@ THREE.OutlineEffect = function ( renderer, parameters ) {
 
 		if ( material.name === 'invisible' ) return;
 
-		var outlineParameters = originalMaterial.userData.outlineParameters;
+		var outlineParameters = originalMaterial.outlineParameters;
 
 		material.skinning = originalMaterial.skinning;
 		material.morphTargets = originalMaterial.morphTargets;
@@ -376,7 +376,7 @@ THREE.OutlineEffect = function ( renderer, parameters ) {
 		} else {
 
 			material.transparent = originalMaterial.transparent;
-			material.visible = originalMaterial.visible;
+			material.visible = false;
 
 		}
 
