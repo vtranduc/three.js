703eb7c41013349a18f198715f1b9723bb01f706
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-16 15:09:47 -0400

    add method to CubeTexture to convert from equirectangular

--- src/renderers/webgl/WebGLBackground.js
+++ src/renderers/webgl/WebGLBackground.js
@@ -27,18 +27,6 @@ function WebGLBackground( renderer, state, objects, premultipliedAlpha ) {
 
 		var background = scene.background;
 
-		// Ignore background in AR
-		// TODO: Reconsider this.
-
-		var xr = renderer.xr;
-		var session = xr.getSession && xr.getSession();
-
-		if ( session && session.environmentBlendMode === 'additive' ) {
-
-			background = null;
-
-		}
-
 		if ( background === null ) {
 
 			setClear( clearColor, clearAlpha );
@@ -60,7 +48,7 @@ function WebGLBackground( renderer, state, objects, premultipliedAlpha ) {
 
 		}
 
-		if ( background && ( background.isCubeTexture || background.isWebGLRenderTargetCube || background.mapping === CubeUVReflectionMapping ) ) {
+		if ( background && ( background.isCubeTexture || background.isWebGLRenderTargetCube || background.mapping === CubeUVReflectionMapping || background.isRenderTargetCubeTexture ) ) {
 
 			if ( boxMesh === undefined ) {
 
