72a33bcf166827d27df04a5586bffc2e890e6b19
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-18 09:33:00 -0400

    added an irradiance map property to physical materials.

--- src/renderers/webgl/WebGLProgram.js
+++ src/renderers/webgl/WebGLProgram.js
@@ -594,6 +594,7 @@ function WebGLProgram( renderer, extensions, code, material, shader, parameters 
 			parameters.envMap ? '#define ' + envMapModeDefine : '',
 			parameters.envMap ? '#define ' + envMapBlendingDefine : '',
 			parameters.envMap ? '#define cubeUV_textureSize (float(' + envMapCubeUVTextureSize + '))' : '',
+			parameters.envIrradianceMap ? '#define USE_IRRADIANCE_MAP' : '',
 			parameters.lightMap ? '#define USE_LIGHTMAP' : '',
 			parameters.aoMap ? '#define USE_AOMAP' : '',
 			parameters.emissiveMap ? '#define USE_EMISSIVEMAP' : '',
