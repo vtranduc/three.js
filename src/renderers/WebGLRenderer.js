840d2524baa6d257b36ad7ffc73da5558f90b26b
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-21 14:56:31 -0400

    add support for directional light cascaded shadow maps.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1901,6 +1901,9 @@ function WebGLRenderer( parameters ) {
 				refreshUniformsCommon( m_uniforms, material );
 				refreshUniformsLambert( m_uniforms, material );
 
+				m_uniforms.zNear.value = camera.near;
+				m_uniforms.zFar.value = camera.far;
+
 			} else if ( material.isMeshPhongMaterial ) {
 
 				refreshUniformsCommon( m_uniforms, material );
@@ -1913,11 +1916,17 @@ function WebGLRenderer( parameters ) {
 
 					refreshUniformsPhong( m_uniforms, material );
 
+					m_uniforms.zNear.value = camera.near;
+					m_uniforms.zFar.value = camera.far;
+
 				}
 
 			} else if ( material.isMeshStandardMaterial ) {
 
 				refreshUniformsCommon( m_uniforms, material );
+
+				m_uniforms.zNear.value = camera.near;
+				m_uniforms.zFar.value = camera.far;
 
 				if ( material.isMeshPhysicalMaterial ) {
 
@@ -1972,6 +1981,9 @@ function WebGLRenderer( parameters ) {
 
 				m_uniforms.color.value.copy( material.color );
 				m_uniforms.opacity.value = material.opacity;
+
+				m_uniforms.zNear.value = camera.near;
+				m_uniforms.zFar.value = camera.far;
 
 			}
 
