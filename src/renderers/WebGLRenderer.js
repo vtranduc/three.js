1128889f0f59a3c6cd342ead7fbf062fad8f6850
Jordan Munroe <jmunroe@spinvfx.com>
2019-10-22 17:09:25 -0400

    add support for skinned meshes without skeletons.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1316,7 +1316,7 @@ function WebGLRenderer( parameters ) {
 
 			} else if ( object.isMesh || object.isLine || object.isPoints ) {
 
-				if ( object.isSkinnedMesh ) {
+				if ( object.isSkinnedMesh && object.skeleton ) {
 
 					// update skeleton only once in a frame
 
