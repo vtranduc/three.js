015bf65f5a013e6cffcd86f5df3e0d06f653b21e
Yomotsu <admin@yomotsu.net>
2019-12-29 14:28:58 +0900

    make is* readonly in d.ts

--- examples/jsm/lines/WireframeGeometry2.d.ts
+++ examples/jsm/lines/WireframeGeometry2.d.ts
@@ -1,13 +1,8 @@
-import {
-	BufferGeometry,
-	Geometry
-} from '../../../src/Three';
+import { BufferGeometry, Geometry } from "../../../src/Three";
 
-import { LineSegmentsGeometry } from './LineSegmentsGeometry';
+import { LineSegmentsGeometry } from "./LineSegmentsGeometry";
 
 export class WireframeGeometry2 extends LineSegmentsGeometry {
-
-	constructor( geometry: Geometry | BufferGeometry );
+	constructor(geometry: Geometry | BufferGeometry);
 	readonly sWireframeGeometry2: boolean;
-
 }
